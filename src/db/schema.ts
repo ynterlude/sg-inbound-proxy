12ba3dcdf7c398af551367b5ae2301ce7ce45690
Jonathan Eldridge <jonathan@ynterlude.com>
2024-12-19 20:10:57 -0800

    Sets proper DB schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,6 @@
-import { pgTable, text, timestamp, integer } from "drizzle-orm/pg-core";
+import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const proxiesTable = pgTable("proxies", {
-  code: text().notNull().unique(),
-  generatedAt: timestamp().defaultNow().notNull(),
-  checkCount: integer().default(0).notNull(),
+  address: text().notNull().unique(),
+  target: text().notNull(),
 });
